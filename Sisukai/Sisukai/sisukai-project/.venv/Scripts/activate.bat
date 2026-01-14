f77b03f99a809c025c19a549f49b5910a61a8e0a
kozima-ecc <2240220@ecc.ac.jp>
2026-01-14 16:25:11 +0900

    Ignore venv

--- Sisukai/Sisukai/sisukai-project/.venv/Scripts/activate.bat
+++ Sisukai/Sisukai/sisukai-project/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=L:\Downloads\Sisukai (1)\Sisukai\sisukai-project\.venv
+set VIRTUAL_ENV=C:\FlutterProjects\NewOkaneLab\Sisukai\Sisukai\sisukai-project\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
